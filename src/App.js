76488cc8dd6820455301baec3870044c7d2b2e94
John Perez <jbperez@saludelectronica.com>
2022-08-17 09:17:52 -0500

    integracion db firebase

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import './App.css';
 import React, { useState } from 'react'
 import { Home } from './page/Home';
-import { Loader }from  './components/utils';
+import { Loader } from './components/utils';
+import './infrastructure/api/fire-credential';
 
 function App() {
-  
-  const [loading,setLoading ] = useState(true);
-  
+
+  const [loading, setLoading] = useState(true);
+
   setTimeout(() => {
     setLoading(false)
   }, 4700);
+
   return (
     <div className="App">
-      
-      { (loading) ? 
-        <Loader/>:
-        <Home nombres={"Jhon & Angie"} fecha={"10-09-22 04:00:00"}/>
-      }
+
+          { (loading) ? 
+              <Loader/>:
+              <Home nombres={"Jhon & Angie"} fecha={"10-09-22 04:00:00"}/>
+          }
+
     </div>
   );
 }
--- src/components/sections/section-confirmed.js
+++ src/components/sections/section-confirmed.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React , {useEffect,useState} from 'react'
 import './header.css'
+import db from '../../infrastructure/api/fire-credential'
 
 export const SectionConfirmed = () => {
+
+    const [clientes, setclientes] = useState([])
+    const getClient = async() => {
+        return await db.collection('clientes').get()
+    }
+
+    useEffect(() => {
+        getClient().then(res=> console.log(res))
+    }, [])
+    
     return (
         <div className='d-flex section-container-back'>
             <div className='header-title'>
                 <h1 className='title-bg'> Confirmar asistencia </h1>
-                <a href="https://www.youtube.com/watch?v=Ea0LE06a-YY&ab_channel=JohnPerez" class="btn btn-success ">Confirmar</a>
+                <a href="" class="btn btn-success ">Confirmar</a>
             </div>
         </div>
 
