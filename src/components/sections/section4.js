ffe90854c50b2b53a11565e02c00cf030c37a68b
John Perez <jbperez@saludelectronica.com>
2023-12-06 11:12:31 -0500

    cambio target

--- src/components/sections/section4.js
+++ src/components/sections/section4.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './section4.css'
+import vestido from '../../img/vestido.png';
 
 export const Section4 = ({participativa}) => {
     
     return (
         <>  
         
-            <div className='section-bg'>
+            <div className='section-bg most-big'>
             <div className="row d-flex justify-content-center mar-custom">
                 
            {/*  <h1 className='title-bg-2'>Agradecemos tú compañia, Desde lejos pero cerca!</h1> */}
@@ -26,6 +27,11 @@ export const Section4 = ({participativa}) => {
                 <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" className="bi bi-envelope color mb-3" viewBox="0 0 16 16">
                     <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
                 </svg>  
+
+                <h1 className='title-bg-2 mt-4'> Vestido Formal </h1>
+                <div style={{display: 'flex', justifyContent: 'center'}}>
+                    <img src={vestido} alt="foto-x2" className='icon-wedding' />
+                </div>
                                {/* 
                 <hr className='division'/> 
                 <div className="col-sm-4">
